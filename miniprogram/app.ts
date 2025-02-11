26b62e91d86d3855fd717b2ed404fd12a62904d7
laifugroup <laifuygj@gmail.com>
2025-02-11 20:54:15 +0800

    user

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,4 +1,8 @@
 // app.ts
+
+import { login as LoginApi } from '../miniprogram/utils/api';
+
+
 App<IAppOption>({
   globalData: {},
   onLaunch() {
@@ -9,7 +13,9 @@ App<IAppOption>({
     // 登录
     wx.login({
       success: res => {
-        console.log("code="+res.code)
+        var code=res.code
+        const res1 =   LoginApi({ username:code,password:code });
+        console.log("code="+res1)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       },
     })
